c8cf3b511b359d642f54f6fa7ebd1c1ba0a80684
RiosVal <riosh.valeria@gmail.com>
2023-10-05 16:13:58 -0500

    agrego archivos de domain

---
+++ Backend/src/main/java/com/Bankcol/BankColApp/domain/Sucursal.java
@@ -0,0 +1,29 @@
+package com.Bankcol.BankColApp.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "sucursales")
+public class Sucursal {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String ciudad;
+
+    @Column(nullable = false, length = 50)
+    private String direccion;
+
+    @Column(nullable = false, length = 20)
+    private String telefono;
+}
