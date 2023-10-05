2148698bf65166ee6f1f498a5682c494fff05dbc
RiosVal <riosh.valeria@gmail.com>
2023-10-05 16:51:24 -0500

    agrego archivos de service

---
+++ Backend/src/main/java/com/Bankcol/BankColApp/mapper/ClienteMapper.java
@@ -0,0 +1,40 @@
+package com.Bankcol.BankColApp.mapper;
+
+import com.Bankcol.BankColApp.domain.Cliente;
+import com.Bankcol.BankColApp.dto.ClienteDTO;
+
+import java.util.List;
+
+public class ClienteMapper {
+    public static Cliente dtoToDomain(ClienteDTO clienteDTO) {
+        return Cliente.builder()
+                .id(clienteDTO.getId())
+                .cedula(clienteDTO.getCedula())
+                .nombre(clienteDTO.getNombre())
+                .apellido(clienteDTO.getApellido())
+                .usuario(clienteDTO.getUsuario())
+                .correo(clienteDTO.getCorreo())
+                .contra(clienteDTO.getContra())
+                .build();
+    }
+
+    public static ClienteDTO domainToDto(Cliente cliente) {
+        return ClienteDTO.builder()
+                .id(cliente.getId())
+                .cedula(cliente.getCedula())
+                .nombre(cliente.getNombre())
+                .apellido(cliente.getApellido())
+                .usuario(cliente.getUsuario())
+                .correo(cliente.getCorreo())
+                .contra(cliente.getContra())
+                .build();
+    }
+
+    public static List<Cliente> dtoToDomainList(List<ClienteDTO> clienteDTOS) {
+        return clienteDTOS.stream().map(ClienteMapper::dtoToDomain).toList();
+    }
+
+    public static  List<ClienteDTO> domainToDtoList(List<Cliente> clientes) {
+        return clientes.stream().map(ClienteMapper::domainToDto).toList();
+    }
+}
