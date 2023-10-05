9f0150d4293add1395ba16cff837003e25beca36
RiosVal <riosh.valeria@gmail.com>
2023-10-05 15:13:47 -0500

    subo backend con springboot

---
+++ Backend/src/main/java/com/Bankcol/BankColApp/BankColAppApplication.java
@@ -0,0 +1,13 @@
+package com.Bankcol.BankColApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankColAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankColAppApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Bankcol/BankColApp/BankColAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bankcol.BankColApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankColAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
